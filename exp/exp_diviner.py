1b208a4b2202df297d334135695364f2f393f670
kim young il <95357946+stockmanager1@users.noreply.github.com>
2023-10-20 14:35:12 +0900

    Update exp_diviner.py

--- exp/exp_diviner.py
+++ exp/exp_diviner.py
@@ -117,8 +117,6 @@ class Exp_Diviner(Exp_Basic):
 
         MSE_records.append(mse_res['loss'])
         MAE_records.append(mae_res['loss'])
-        mse_res['net_out'] = test_dataset.inverse_label_transform(mse_res['net_out'])
-        mae_res['net_out'] = test_dataset.inverse_label_transform(mae_res['net_out'])
 
         predict_values.append(mse_res['net_out'])
         original_values.append(mse_res['label'].detach().cpu().numpy())
