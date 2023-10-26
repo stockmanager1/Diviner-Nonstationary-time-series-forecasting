b6b525b62c1ff5350feb1664d0af75086c2d5a79
kim young il <95357946+stockmanager1@users.noreply.github.com>
2023-10-26 19:55:21 +0900

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -38,12 +38,8 @@ class DatasetETT(DatasetBasic):
         '''
         #! 'raw' means the whole data of the corresponding mode
         df_raw = self.df_raw
-        if self.mode_index == 1 or self.mode_index == 0:
-            border1, border2= self.border1s[self.mode_index], self.border2s[self.mode_index]
-            border1_train, border2_train = self.border1s[0], self.border2s[0]
-        elif self.mode_index == 2:
-            border1, border2= 0, len(df_raw)
-            border1_train, border2_train = 0, len(df_raw)
+        border1, border2= self.border1s[self.mode_index], self.border2s[self.mode_index]
+        border1_train, border2_train = self.border1s[0], self.border2s[0]
 
         #Get data_input
         df_raw_input, df_raw_label = df_raw[self.features], df_raw[[self.target]]
